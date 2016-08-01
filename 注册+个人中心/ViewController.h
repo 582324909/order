b45c1ac73c7f1ddbb82cf8a30a0b013ad3a1572b
brother <zhangweiwei@zhangweiwei.local>
2016-08-01 11:16:00 +0800

    Initial Commit

---
+++ 注册+个人中心/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  注册+个人中心
+//
+//  Created by 张伟伟 on 16/8/1.
+//  Copyright © 2016年 张伟伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 注册+个人中心/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  注册+个人中心
+//
+//  Created by 张伟伟 on 16/8/1.
+//  Copyright © 2016年 张伟伟. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
